a725f87d3e0ab33f2f8d1482f9b846e7561792f4
Alex <aleksmaliszews@gmail.com>
2021-08-22 17:17:43 +0200

    correction :b

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
 const port = 4000;
 app.listen(4000, () => console.log("app is running :D"));
 app.use(express.static("public/"));
+app.use(express.json());
+
+
+//send email
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'ttes77673@gmail.com',
+            pass: 'Omal1234'
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'macmrcopywriter@gmail.com',
+        subject: `Wiadomość od ${req.body.email}: ${req.body.subject}`,
+        html: `<h2 style="text-transform:uppercase;letter-spacing:2px;">Uwaga nowe zlecenie!</h2><p>Treść wiadomości: <br />${req.body.message}</p><p>Numer telefonu: ${req.body.phone_number}<br /> Email: ${req.body.email} <br /> Imię: ${req.body.firstname} <br /> Nazwisko: ${req.body.lastname}</p>`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) =>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent:' + info.response);
+            res.send('success');
+        }
+    })
+})
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -24,13 +24,13 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // OKIENKO POJAWIAJĄCE SIĘ PRZY SEKCJI 'USŁUGI' PO KLIKNIĘCIU PRZYCISKU SERVICES_BUTTON
-$('.services_button').on('click', function(){
-    $(this).siblings('.services_modal').addClass('active-modal');
-});
+// $('.services_button').on('click', function(){
+//     $(this).siblings('.services_modal').addClass('active-modal');
+// });
 
-$('.services_modal-close').on('click', function(){
-    $(this).parent('.services_modal-content').parent('.services_modal').removeClass('active-modal');
-})
+// $('.services_modal-close').on('click', function(){
+//     $(this).parent('.services_modal-content').parent('.services_modal').removeClass('active-modal');
+// })
 
 // POJAWIANIE SIĘ PRELOADERA I JEGO ZANIKANIE
 
@@ -84,12 +84,46 @@ $('.portfolio .button-container .btn').click(function(){
     }
 });
 
-// PRZYCISK LOAD MORE
+//WYSYŁANIE WIADOMOŚCI 
 
-// $('.box').slice(0, 9).show()
-// $('.przy').on('click', function(){
-//     $('.box:hidden').slice(0, 3).slideDown()
-//     if($('.box:hidden').length == 0){
-//         $('.przy').fadeOut('slow')
-//     }
-// })
+let firstname = document.getElementById('first_name');
+let lastname =  document.getElementById('last_name');
+let email = document.getElementById('email');
+let phone_number = document.getElementById('phone_number');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+const contactForm = document.querySelector('.formBox');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let formData = {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        email: email.value,
+        phone_number: phone_number.value,
+        subject: subject.value,
+        message: message.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert(`Wiadomość została wysłana :)`);
+            firstname.value = '';
+            lastname.value = '';
+            email.value = '';
+            phone_number.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Coś poszło nie tak :/ Spróbuj jeszcze raz po pewnym czasie!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+})
+
